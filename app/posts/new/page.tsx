5b6d93c8feee057d9931bd6126a5955d757eed17
Marcus <marcusicaromc@gmail.com>
2023-09-23 07:32:46 -0300

    wip post articles

--- app/posts/new/page.tsx
+++ app/posts/new/page.tsx
@@ -3,24 +3,48 @@ import { Editor } from '@tinymce/tinymce-react';
 import React, { useRef } from 'react';
 export default function Page() {
   const editorRef = useRef(null);
+  const [title, setTitle] = React.useState('');
   const log = () => {
     if (editorRef.current) {
       console.log(editorRef.current);
     }
   };
+  const postArticle = () => {
+    try {
+      fetch('http://localhost:3002/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+          title: title,
+          content: log,
+        }),
+        headers: {
+          'content-type': 'application/json',
+        },
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
-    <>
+    <div className='flex flex-col gap-2'>
+      <div className='flex flex-col'>
+        <label htmlFor='title'>Title:</label>
+        <input
+          value={title}
+          onChange={(val) => setTitle(val.target.value)}
+          type='text'
+          name='title'
+          id='title'
+        />
+      </div>
       <Editor
+        id='editor'
+        apiKey='z8fg4722m7vi4i7ntjdxl6ouumofczoeac22mtbrybzpfsbi'
         onInit={(evt, editor) => (editorRef.current = editor)}
         initialValue='<p>This is the initial content of the editor.</p>'
         init={{
           height: 500,
           menubar: false,
-          plugins: [
-            'advlist autolink lists link image charmap print preview anchor',
-            'searchreplace visualblocks code fullscreen',
-            'insertdatetime media table paste code help wordcount',
-          ],
           toolbar:
             'undo redo | formatselect | ' +
             'bold italic backcolor | alignleft aligncenter ' +
@@ -30,7 +54,9 @@ export default function Page() {
             'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
         }}
       />
-      <button onClick={log}>Log editor content</button>
-    </>
+      <button onClick={log} className='justify-start flex'>
+        Log editor content
+      </button>
+    </div>
   );
 }
